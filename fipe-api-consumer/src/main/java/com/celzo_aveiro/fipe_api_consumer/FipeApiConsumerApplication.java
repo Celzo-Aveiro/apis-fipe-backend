6be3697a0928610ed6b3ac8ebe49edd4023b7540
celzo-aveiro <celzoismaelaveirofilho@gmail.com>
2026-05-01 17:26:29 -0300

    chore: commit inicial de ambas as apis

---
+++ fipe-api-consumer/src/main/java/com/celzo_aveiro/fipe_api_consumer/FipeApiConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.celzo_aveiro.fipe_api_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeApiConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipeApiConsumerApplication.class, args);
+	}
+
+}
---
+++ fipe-api-consumer/src/test/java/com/celzo_aveiro/fipe_api_consumer/FipeApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celzo_aveiro.fipe_api_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeApiConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
